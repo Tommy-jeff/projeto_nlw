59f7f8a19475f6e968d2e8f3f5cdb28ea9a7e3c6
Thomas <thomasjefferson.dev@gmail.com>
2025-02-20 22:25:55 -0300

    feat: Implementing subscribers repository

---
+++ src/model/repositories/subscribers_repository.py
@@ -0,0 +1,45 @@
+from src.model.configs.connection import DbConnectionHandler
+from src.model.entities.inscritos import Inscritos
+
+class SubscribersRepository:
+    def insert(self, subscriber_info: dict) -> None:
+        with DbConnectionHandler() as db:
+            try:
+                new_subscriber = Inscritos(
+                    nome=subscriber_info.get("name"),
+                    email=subscriber_info.get("email"),
+                    link=subscriber_info.get("link"),
+                    evento_id=subscriber_info.get("evento_id"),
+                    )
+                db.session.add(new_subscriber)
+                db.session.commit()
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
+            
+    def select_subscriber(self, email: str, evento_id: int) -> Inscritos:
+        with DbConnectionHandler() as db:
+            data = (
+                db.session.query(Inscritos)
+                .filter(
+                    Inscritos.email == email,
+                    Inscritos.evento_id == evento_id)
+                .limit(1)
+                .one_or_none()
+            )
+
+            return data
+        
+    def delete_subscriber(self, id: int) -> int:
+        with DbConnectionHandler() as db:
+            try:
+                delete_subcriber = db.session.get_one(
+                    Inscritos, 
+                    id
+                    )
+
+                db.session.delete(delete_subcriber)
+                db.session.commit()
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
---
+++ src/model/repositories/subscribers_repository_test.py
@@ -0,0 +1,31 @@
+from .subscribers_repository import SubscribersRepository
+import pytest
+
+@pytest.mark.skip("Insert in db")
+def test_insert_subscriber():
+    subscriber_info = {
+        "name" : "Teste2",
+        "email" : "teste2@email.com",
+        "evento_id" : 2
+    }
+
+    subs_repo = SubscribersRepository()
+    subs_repo.insert(subscriber_info)
+
+@pytest.mark.skip("Select in db")
+def test_select_subscriber():
+    subscriber_email = "teste2@email.com"
+    evanto_id = 2
+
+    subs_repo = SubscribersRepository()
+    subs = subs_repo.select_subscriber(subscriber_email, evanto_id)
+
+    print(f"\nconsulta: {subs}")
+    print(f"\nconsulta nome: {subs.nome}")
+
+# @pytest.mark.skip("Delete in db")
+def test_delete_subscriber():
+    subscribe_id = 2
+
+    subs_repo = SubscribersRepository()
+    subs_repo.delete_subscriber(subscribe_id)
